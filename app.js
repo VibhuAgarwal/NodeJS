b36c166fdcccda84e7a56dd9da1a856e321eac17
Vibhor Agarwal <agarwalvibhor930@gmail.com>
2024-01-25 01:17:14 +0530

    ExpressJS Basics

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use("/", (req, res, next) => {
+//   console.log("This always runs");
+//   res.status(200).send("<h1>This alway runs</h1>");
+//   next();
+// });
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add product</button></form>'
+  );
 });
-app.use((req, res, next) => {
-  console.log("In another middleware");
+
+app.post("/product", (req, res, next) => {
+  const { body } = req;
+  console.log(req.body.title);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   res.status(200).send("<h1>Hello from express</h1>");
 });
 
