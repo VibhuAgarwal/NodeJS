c019707d4f7a146ff106310fb12175a0f59fdb9e
Vibhor Agarwal <agarwalvibhor930@gmail.com>
2024-02-08 17:39:07 +0530

    Added product, sjop, error controllers

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,21 @@
+const products = [];
+
 exports.getAddProduct = (req, res, next) => {
-  const products = adminData.products;
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    formCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect("/");
+};
+
+exports.getProducts = (req, res, next) => {
   res.render("shop", {
     prods: products,
     pageTitle: "Shop",
--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,10 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../utils/path");
-
-const adminData = require("./admin");
+const productsController = require("../controllers/products");
 
 const router = express.Router();
 
-router.get("/");
+router.get("/", productsController.getProducts);
 
 module.exports = router;
